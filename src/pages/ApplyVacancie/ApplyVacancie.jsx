82cac1deb83b0da4fb826dbc8c34444b069b95ee
HectorGinory <hectorginorydevs@gmail.com>
2023-06-13 14:39:54 +0200

    feat: Added inputs to vacancies questions

--- src/pages/ApplyVacancie/ApplyVacancie.jsx
+++ src/pages/ApplyVacancie/ApplyVacancie.jsx
@@ -8,9 +8,7 @@ import { userData } from "../userSlice";
 
 const ApplyVacancie = () => {
   const userRdxData = useSelector(userData);
-  const [credentials, setCredentials] = useState({
-    answer_one: "",
-  });
+  const [credentials, setCredentials] = useState({});
   const vacancieId = useParams().id;
   const [vacancie, setVacancie] = useState({});
 
@@ -25,17 +23,63 @@ const ApplyVacancie = () => {
     getVacancieById(vacancieId).then((res) => {
       setVacancie(res.data);
       console.log(res.data);
-      console.log(userRdxData)
+      console.log(userRdxData);
     });
-    getUserByUserName(userRdxData.user.username).then((res) => {
-
-    })
+    getUserByUserName(userRdxData.user.username).then((res) => {});
   }, []);
   return (
     <div className="">
       {vacancie._id ? (
         <div className="vacancie-form-container">
           <h2>Rellena el formulario</h2>
+          {vacancie.question_one && (
+            <label className="flex align-c f-column justify-c justify-sb">
+              <div className="flex f-column justify-c original-info">
+                <p>{vacancie.question_one}</p>
+              </div>
+              <InputText
+                type={"text"}
+                className={"input"}
+                placeholder={"Insert name"}
+                name={"answer_one"}
+                handler={credentialsHandler}
+                required={true}
+                value={credentials.answer_one}
+              />
+            </label>
+          )}
+          {vacancie.question_two && (
+            <label className="flex align-c f-column justify-c justify-sb">
+              <div className="flex f-column justify-c original-info">
+                <p>{vacancie.question_two}</p>
+              </div>
+              <InputText
+                type={"text"}
+                className={"input"}
+                placeholder={"Insert name"}
+                name={"answer_two"}
+                handler={credentialsHandler}
+                required={true}
+                value={credentials.answer_two}
+              />
+            </label>
+          )}
+          {vacancie.question_three && (
+            <label className="flex align-c f-column justify-c justify-sb">
+              <div className="flex f-column justify-c original-info">
+                <p>{vacancie.question_three}</p>
+              </div>
+              <InputText
+                type={"text"}
+                className={"input"}
+                placeholder={"Insert name"}
+                name={"answer_three"}
+                handler={credentialsHandler}
+                required={true}
+                value={credentials.answer_three}
+              />
+            </label>
+          )}
         </div>
       ) : (
         <div className="flex align-c justify-c">
