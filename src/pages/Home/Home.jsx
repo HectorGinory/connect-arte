293f6f482e9d528f921b738ce041c814e38392cf
HectorGinory <hectorginorydevs@gmail.com>
2023-06-16 19:18:25 +0200

    feat: Added Spinner to Homepage

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,6 +7,7 @@ import ButtonIcon from '../../common/ButtonIcon/ButtonIcon'
 import {AiOutlineLogin, AiOutlineArrowRight} from 'react-icons/ai'
 
 import './Home.css'
+import Spinner from '../../common/Spinner/Spinner'
 const Home = () => {
   const userRdxData = useSelector(userData)
   const [vacancies, setVacancies] = useState([])
@@ -28,40 +29,46 @@ const Home = () => {
   }, [])
   return (
     <div className='flex f-column home-container'>
-      <h1>Bienvenid@ a Connect-arte!</h1>
-      <p>Aquí te dejo algunas ofertas de empleo, que pueden interesarte!</p>
-      {!userRdxData.user.username ?
-      <p>Inicia sesión o registrate para poder aplicar a ellas</p> :
-      <p>Añade más palabras a tu lista de intereses para tener ofertas más personalizadas</p>
-      }
-      <div className='vacancies-container'>
-      {vacancies.map((vacancie, index) => {
-        return (
-          <div key={index} className='vacancie'>
-          <div className='info-vacancie'>
-          <p>{vacancie.charge_name} - {vacancie.location}</p>
+    <h1>Bienvenid@ a Connect-arte!</h1>
+    <p>Aquí te dejo algunas ofertas de empleo, que pueden interesarte!</p>
+    {!userRdxData.user.username ?
+    <p>Inicia sesión o registrate para poder aplicar a ellas</p> :
+    <p>Añade más palabras a tu lista de intereses para tener ofertas más personalizadas</p>
+    }
+    <div className='vacancies-container'>
+    {vacancies.length > 0 ?
+    <>
+    {vacancies.map((vacancie, index) => {
+      return (
+        <div key={index} className='vacancie'>
+        <div className='info-vacancie'>
+        <p>{vacancie.charge_name} - {vacancie.location}</p>
+        {!userRdxData.user.username ?
+          <p> {vacancie.created_by.username}</p> :
+          <a href={`user/${vacancie.created_by.username}`}>
+            <p> {vacancie.created_by.username}</p>
+          </a>
+        }
+        </div>
+        <div className='btn-container'>
           {!userRdxData.user.username ?
-            <p> {vacancie.created_by.username}</p> :
-            <a href={`user/${vacancie.created_by.username}`}>
-              <p> {vacancie.created_by.username}</p>
-            </a>
+          <>
+          <ButtonIcon path={'login'} ReactIcon={AiOutlineLogin} text={'Inicia sesión'}/>
+          </>:
+          <>
+          <ButtonIcon path={`vacancieDetail/${vacancie._id}`} ReactIcon={AiOutlineArrowRight} text={'Ir a la oferta'}/>
+          </>
           }
-          </div>
-          <div className='btn-container'>
-            {!userRdxData.user.username ?
-            <>
-            <ButtonIcon path={'login'} ReactIcon={AiOutlineLogin} text={'Inicia sesión'}/>
-            </>:
-            <>
-            <ButtonIcon path={`vacancieDetail/${vacancie._id}`} ReactIcon={AiOutlineArrowRight} text={'Ir a la oferta'}/>
-            </>
-            }
-          </div>
-          </div>
-        )
-      })}
-      </div>
+        </div>
+        </div>
+      )
+    })}
+    </>:
+    <>
+      <Spinner/>
+    </>}
     </div>
+  </div>
   )
 }
 
